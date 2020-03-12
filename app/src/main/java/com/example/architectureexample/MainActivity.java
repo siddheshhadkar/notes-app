0f887f59a595668e179a83484063fca2031877e1
siddheshhadkar <siddheshhadkar07@gmail.com>
2020-03-12 10:05:36 +0530

    Database config completed using Room persistence library

---
+++ app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.architectureexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "Test Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
